8b991cedbe1c8143af17e58c08e4a963dd409f02
Yifei Yin <yifeiyin@foxmail.com>
2016-12-20 00:18:38 -0800

    Bugs fixed
    Print functions need more improvements.

--- Problem.h
+++ Problem.h
@@ -16,7 +16,7 @@
 class Problem
 {
 private:
-    const int decimal_setting = 2;
+    const int decimalSetting = 2;
     const double g = 9.8;
     int type = -1;
     double miu_k = 0.0;
@@ -26,34 +26,34 @@ private:
     double time = 0.0;
     double velocity = 0.0;
     
-    double NumG ( double min, double max, int setting = -1)
+    double NumG ( double min, double max, int deci = -1)
     {
-        if (setting == -1)
-            setting = decimal_setting;
+        if (deci == -1)
+            deci = decimalSetting;
         
-        int i_min = Round(min, setting) * pow(10, setting);
-        int i_max = Round(max, setting) * pow(10, setting);
+        min *= pow(10, deci);
+        max *= pow(10, deci);
         
         std::srand((int)std::time(NULL)+rand());
         
-        double output = rand() % (i_max - i_min) + i_min;
-        output = Round(output / pow(10, setting), setting);
+        double output = rand() % (int)(max - min) + min;
+        output /= pow(10, deci);
         return output;
     }
     
-    double Round (double number, int setting = -1)
+    double Round (double number, int deci = -1)
     {
-        if (setting == -1)
-            setting = decimal_setting;
+        if (deci == -1)
+            deci = decimalSetting;
         
-        double output = number * pow(10, setting);
+        double output = number * pow(10, deci);
         
         if (output - (int)output >= 0.5)
             output = (int)output + 1;
         else
             output = (int)output;
         
-        output /= pow(10,setting);
+        output /= pow(10,deci);
         
         return output;
     }
@@ -61,8 +61,8 @@ private:
 public:
     Problem()
     {
-        miu_k = NumG(0,1);
-        miu_s = NumG(miu_k+0.1,1);
+        miu_k = NumG(0,0.99);
+        miu_s = NumG(miu_k+0.01,1);
         lhype = NumG(20,100);
         
         theta = atan(miu_s);
@@ -72,20 +72,48 @@ public:
                     );
         velocity = g * (sin(theta) - miu_k * cos(theta)) * time;
         theta = theta / 3.14159265 * 180;
-        SetProblem();
+        SetProblemType();
     }
     
-    void SetProblem(int type = -1)
+    void SetProblemType(int typ = -1)
     {
-        if (type == -1)
+        if (typ == -1)
+        {
+            type = NumG(1,5,0);
+        }
+        else
+        {
+            type = typ;
+        }
+    }
+    
+    void PrintProblem()
+    {
+        if (type == 1)
         {
             
+        }
+        else if (type == 2)
+        {
+            
+        }
+        else if (type == 3)
+        {
+            
+        }
+        else if (type == 4)
+        {
+            
+        }
+        else
+        {
+            std::cout << "Unexpected error.\n";
         }
     }
     
     void PrintAll()
     {
-        std::cout << "Problem: " << std::endl
+        std::cout << "Unrounded numbers: " << std::endl
         << "miu_k = " << miu_k << std::endl
         << "miu_s = " << miu_s << std::endl
         << "lhype = " << lhype << std::endl
@@ -93,8 +121,47 @@ public:
         << "time  = " << time << std::endl
         << "veloc = " << velocity << std::endl
         << "----------------" << std::endl;
+        
+        std::cout.precision(3);
+        std::cout << std::fixed;
+        
+        std::cout << "Problem: " << std::endl
+        << "\t| miu_k\t| miu_s\t| lhype\t| theta\t| time\t| velocity\t|"
+        << std::endl
+        << " \t+----------\t+----------\t+----------\t+----------\t+----------\t+----------\t+"
+        << std::endl
+        << "\t| "
+        << miu_k << "\t| "
+        << miu_s << "\t| "
+        << lhype << "\t| "
+        << theta << "\t| "
+        << time << "\t| "
+        << velocity << "\t|"
+        << std::endl
+        << "\n================\n" << std::endl;
+        
+        std::cout << std::defaultfloat;
     }
     
+    void PrintContinuously()
+    {
+        std::cout.precision(3);
+        std::cout << std::fixed;
+        
+        std::cout
+        << " \t+----------\t+----------\t+----------\t+----------\t+----------\t+----------\t+"
+        << std::endl
+        << "\t| "
+        << miu_k << "\t| "
+        << miu_s << "\t| "
+        << lhype << "\t| "
+        << theta << "\t| "
+        << time << "\t| "
+        << velocity << "\t|"
+        << std::endl;
+        
+        std::cout << std::defaultfloat;
+    }
 };
 
 #endif /* Problem_h */
--- main.cpp
+++ main.cpp
@@ -10,10 +10,11 @@
 #include "Problem.h"
 
 int main() {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 30; i++)
     {
         Problem problem;
-        problem.PrintAll();
+        if (i == 0) problem.PrintAll();
+        else problem.PrintContinuously();
     }
     
     return 0;
