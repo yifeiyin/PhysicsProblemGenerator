d0843c0420f43ae2ffdea3d362f7140026a0ec4b
Yifei Yin <yifeiyin@foxmail.com>
2016-12-21 17:10:19 -0800

    ...

--- main.cpp
+++ main.cpp
@@ -10,17 +10,43 @@
 #include "Problem.h"
 #include "ExportAsMD.h"
 
-int main() {
+int main()
+{
+    using namespace std;
     
-    Problem problems[10];
-    ExportAsMD(10, problems, "MD.md", 1);
-    ExportAsMD(10, problems, "MD-KEYS.md", 2);
+    cout << "Number of questions?\n> ";
     
-//    for (int i = 0; i < 10; i++)
-//    {
-//        Problem problem;
-//        problem.PrintProblem(i + 1, "exporting7.csv");
-//    }
+    int nProblem;
+    cin >> nProblem;
+    
+    cout << "Do you want answer key?\n[Y/N]> ";
+    char ch;
+    cin >> ch;
+    cin.ignore(99999, '\n');
+    
+    cout << "Name of the file?\n> ";
+    string fileN;
+    getline(cin,fileN);
+    
+    if ((fileN.find(".md") != fileN.length() - 3) && (fileN.find(".MD") != fileN.length() - 3))
+    {
+        fileN += ".md";
+    }
+    
+    string fileAnswer;
+    fileAnswer  = fileN.substr(0,fileN.length() - 3) + "-ANSWER.md";
+    
+    cout << endl << endl;
+    cout << "Number of questions: " << nProblem << endl;
+    cout << "File name: " << fileN << endl;
+    cout << "Answer file name: " << fileAnswer << endl;
+    cout << endl;
+    
+    Problem * problems = new Problem [nProblem];
+    ExportAsMD(nProblem, problems, fileN, 1);
+    
+    if(ch == 'Y' || ch == 'y')
+        ExportAsMD(nProblem, problems, fileAnswer, 2);
     
     std::cout << "Done.\n";
     
